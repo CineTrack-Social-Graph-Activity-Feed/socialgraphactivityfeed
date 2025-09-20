6c04c9a65fbd7bb85ea61896083075d427fdb20d
nicoles-aversa <naversa@uade.edu.ar>
2025-09-20 16:19:50 -0300

    prueba3

--- Frontend/front-cinetrack/src/App.jsx
+++ Frontend/front-cinetrack/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import FeedPrincipal from "./pages/FeedPrincipal/FeedPrincipal";
 import MiActividad from "./pages/MiActividad/MiActividad";
 import Follows from "./pages/Follows/Follows";
@@ -14,10 +14,17 @@ function App() {
     <>
       <UserContext.Provider value={{ userId: testUserId }}>
         <Routes>
+          {/* Redirigir /index.html a la ruta raíz */}
+          <Route path="/index.html" element={<Navigate to="/" replace />} />
+
+          {/* Tus rutas existentes */}
           <Route path="/" element={<FeedPrincipal />}></Route>
           <Route path="/mi-actividad" element={<MiActividad />}></Route>
           <Route path="/follows" element={<Follows />}></Route>
           <Route path="/buscar-amigos" element={<BuscarAmigos />}></Route>
+
+          {/* Ruta para manejar 404 - debe estar al final */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </UserContext.Provider>
     </>
