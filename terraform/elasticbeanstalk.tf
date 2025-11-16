e120ab453238de26f43b6ac93a7d1fdf4b1fec7f
nicoles-aversa <naversa@uade.edu.ar>
2025-11-16 01:29:53 -0300

    fix: usar dominios personalizados (socialgraph.cine-track.com.ar y socialgraphbe.cine-track.com.ar)

--- terraform/elasticbeanstalk.tf
+++ terraform/elasticbeanstalk.tf
@@ -58,7 +58,7 @@ resource "aws_elastic_beanstalk_environment" "app_env" {
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "CORS_ORIGINS"
-    value     = "https://${aws_cloudfront_distribution.frontend.domain_name}"
+    value     = "https://socialgraph.cine-track.com.ar"
   }
  
   setting {
